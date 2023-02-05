bf206c3ff320d30ec3398486dcefaab42291a0b4
Luis O <30819865+hlcxpl@users.noreply.github.com>
2023-02-04 22:08:27 -0300

    M7D6

---
+++ Backend/consultas.js
@@ -0,0 +1,45 @@
+const { Pool } = require('pg')
+const bcrypt = require('bcryptjs')
+const { format } = ("util")
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    password: 'luispost',
+    database: 'softjobs',
+    allowExitOnIdle: true
+})
+
+const resgistrarUsuario = async ({ email, password, rol, lenguage }) => {
+    const passwordEncryptada = bcrypt.hashSync(password)
+    password = passwordEncryptada
+    const values = [email, passwordEncryptada, rol, lenguage]
+    const consulta = " insert into usuarios values(DEFAULT, $1, $2, $3,$4)"
+    await pool.query(consulta, values)
+}
+
+const verificarCredenciales = async ({ email, password }) => {
+    const values = [email]
+    consulta = "select * from usuarios where email = $1"
+    const { rows: [usuario], rowCount } = await pool.query(consulta, values)
+    const { password: passwordEncryptada } = usuario
+    const passwordiscorrect = bcrypt.compareSync(password, passwordEncryptada)
+    if (!passwordiscorrect || !rowCount) throw { code: 401, message: "Email y contraseña o Contraseña Incorrecta" }
+}
+
+const obtenerUsuario = async (email) => {
+    const values = [email]
+    consulta = "Select * from usuarios where email = $1"
+    const { rows: usuario } = await pool.query(consulta, values)
+    return usuario[0]
+}
+
+const mostrarInfo = (usuario) => {
+    const info = {
+        "Email": usuario.email,
+        "Rol": usuario.rol,
+        "lenguage": usuario.lenguage
+    }
+    return info
+}
+module.exports = { resgistrarUsuario, verificarCredenciales, obtenerUsuario, mostrarInfo }
---
+++ Backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { reportarConsulta } = require('./middlewares/reporte_consulta')
+const { resgistrarUsuario, verificarCredenciales, obtenerUsuario, mostrarInfo } = require('./consultas')
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3000, () => console.log('SERVER ON'))
+
+app.post('/usuarios', reportarConsulta, async (req, res) => {
+    try {
+        const usuario = req.body
+        await resgistrarUsuario(usuario)
+        res.send('Usuario Registrado con Éxito')
+    } catch (error) {
+        res.status(error.code || 500).send(error)
+    }
+})
+
+app.post('/login', reportarConsulta, async (req, res) => {
+    try {
+        const { email } = req.body
+        const usuario = req.body
+        await verificarCredenciales(usuario)
+        const token = jwt.sign({ email }, "a-z_A-Z")
+        res.send(token)
+        res.send("usuario logueado")
+    } catch (error) {
+        res.status(error.code || 500).send(error)
+    }
+})
+app.get('/usuarios', reportarConsulta, async (req, res) => {
+    const Authorization = req.header("Authorization")
+    const token = Authorization.split("Bearer ")[1]
+    jwt.verify(token, "a-z_A-Z")
+    const { email } = jwt.decode(token)
+    const usuario = await obtenerUsuario(email)
+    const info = mostrarInfo(usuario)
+    res.json(info)
+})
